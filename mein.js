ad15217fa5b74260557e60044118bdcaf4e4db96
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-12-01 14:49:44 +0500

    fulse

--- mein.js
+++ mein.js
@@ -1,12 +1,18 @@
+// let age = 19;   //data-type = number;
+// let myName = "samariddin";  //data-type = string;
+// let bool = false  // data-type = undifinet
+// console.log(number);
+// console.log(a)    //hech nima berilamasa  data-type = null;
 
-function ismqowiw (){
-    let ism = prompt("ismingizni yozing");
-    let familya = prompt('familyang nima ')
+
+
+//object
+
+let abbos = {
+    name:"samar",//string
+    age:19, //number
+    isMarried:false
 
 }
-01
-console.log(prompt);
 
-storage.setItem('ism', ism);
 
-let storage = window.localStorage
